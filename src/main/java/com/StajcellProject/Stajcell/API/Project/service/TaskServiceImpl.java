1a25fc497505df7c85ed06b8993bbf1f18eddfff
tohli <yusuf.coskun1@boun.edu.tr>
2023-08-31 14:48:27 +0300

    Cache tükenme mekanizması kuruldu.

--- src/main/java/com/StajcellProject/Stajcell/API/Project/service/TaskServiceImpl.java
+++ src/main/java/com/StajcellProject/Stajcell/API/Project/service/TaskServiceImpl.java
@@ -2,8 +2,10 @@ package com.StajcellProject.Stajcell.API.Project.service;
 
 import com.StajcellProject.Stajcell.API.Project.model.Task;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,10 +22,16 @@ public class TaskServiceImpl implements TaskService {
         }
         @Cacheable("tasksCache")
         public List<Task> getAllTasks() {
-            System.out.println("Executing getAllTasks() method");
+            System.out.println("Getting tasks from API...");
             ResponseEntity<Task[]> response = restTemplate.getForEntity(apiUrl, Task[].class);
             Task[] tasks = response.getBody();
             return Arrays.asList(tasks);
+        }
+
+        @CacheEvict(value = "tasksCache", allEntries = true)
+        @Scheduled(fixedRateString = "${caching.spring.taskTTL}")
+        public void emptyTasksCache() {
+            System.out.println("Emptying cache...");
         }
     }
 

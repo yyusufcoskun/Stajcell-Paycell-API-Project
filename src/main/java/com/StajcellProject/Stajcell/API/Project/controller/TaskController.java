bd878e80a1188d7ccc4b39b32fcf5de11a4112cd
tohli <yusuf.coskun1@boun.edu.tr>
2023-09-04 16:15:02 +0300

    Cache'i manuel silme seçeneği eklendi.Cache manuel silindiğinde index'e götürme eklendi (QoL).

--- src/main/java/com/StajcellProject/Stajcell/API/Project/controller/TaskController.java
+++ src/main/java/com/StajcellProject/Stajcell/API/Project/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 import java.util.List;
@@ -21,10 +22,21 @@ public class TaskController {
     }
 
     @GetMapping("/")
+    public String viewHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/tasks")
     public String listTasks(Model model) {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "task-list";
+    }
+
+    @PostMapping("tasks/clear")
+    public String clearAllCacheValues(Model model) {
+        taskService.clearAllCacheValues("tasksCache");
+        return "redirect:/";
     }
 
 }
